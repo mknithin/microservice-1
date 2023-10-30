1dc426b24173013df6ffcd74748f26fa0fdcb75f
mknithin <sendtonithin@gmail.com>
2023-10-30 16:24:25 +0400

    added say endpoint

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/say")
+async def get_say():
+    return {"message": "Say something!"}
+
 @app.get("/wish")
 async def get_wish():
     return {"message": "Hello there good morning!"}
